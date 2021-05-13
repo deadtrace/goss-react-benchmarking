aa5b140d36fbc2eaa0c2a56f302eaa4078fd9c49
Stanislav Katyushin <justtrace17@gmail.com>
2021-05-13 16:06:23 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { getData } from "./helpers/data";
+import { Table, Tbody, Tr, Td } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const fetchedData = await getData();
+            setData(fetchedData);
+        };
+        fetchData();
+    }, []);
+
+    if (data.length === 0) return null;
+
+    return (
+        <ChakraProvider>
+            <Table>
+                <Tbody>
+                    {data.map((item) => (
+                        <Tr key={item.id}>
+                            <Td>{item.id}</Td>
+                            <Td>{item.FirstName}</Td>
+                            <Td>{item.LastName}</Td>
+                        </Tr>
+                    ))}
+                </Tbody>
+            </Table>
+        </ChakraProvider>
+    );
 }
 
 export default App;
